85e0c934f6370841b1228b0ce90f545de3edd071
Jia-Wei-Liao <sam23582211@gmail.com>
2023-11-26 19:32:22 +0800

    Stop git action.

--- tests/run_all_test.sh
+++ tests/run_all_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash run.sh "${1}" "${2}" "${3}" "${4}"
-bash tests/test_ppl.sh "${1}" "${2}" "${3}"
+# bash run.sh "${1}" "${2}" "${3}" "${4}"
+# bash tests/test_ppl.sh "${1}" "${2}" "${3}"
