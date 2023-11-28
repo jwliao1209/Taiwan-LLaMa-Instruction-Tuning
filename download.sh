c51897c2d3ead8db4c10ad4f785e789a091f34a6
Jia-Wei-Liao <sam23582211@gmail.com>
2023-11-28 20:35:13 +0800

    Comment download data and pretrain.

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # data
-if [ ! -d data ]; then
-    gdown https://drive.google.com/uc?id=113qrDhnThl9xbYyej-Qe2eKiAm7C-VdS -O data.zip
-fi
+# if [ ! -d data ]; then
+#     gdown https://drive.google.com/uc?id=113qrDhnThl9xbYyej-Qe2eKiAm7C-VdS -O data.zip
+# fi
 
 # pretrain
-if [ ! -d pretrain ]; then
-    gdown https://drive.google.com/uc?id=1qlgYaHXzCLmt_EpdIIIc0i4qu83rfqhp -O pretrain.zip
-fi
+# if [ ! -d pretrain ]; then
+#     gdown https://drive.google.com/uc?id=1qlgYaHXzCLmt_EpdIIIc0i4qu83rfqhp -O pretrain.zip
+# fi
 
 # checkpoint
 if [ ! -d adapter_checkpoint ]; then
