5653df6e547b25c2a874018eb6694965395e67ff
Jia-Wei-Liao <sam23582211@gmail.com>
2023-11-29 14:11:56 +0800

    Move unzip to download.sh.

--- download.sh
+++ download.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-# data
+# Prepare data
 # if [ ! -d data ]; then
 #     gdown https://drive.google.com/uc?id=113qrDhnThl9xbYyej-Qe2eKiAm7C-VdS -O data.zip
 # fi
 
-# pretrain
+# if [ ! -d data ]; then
+#     unzip data.zip
+# fi
+
+
+# Prepare pretrain
 # if [ ! -d pretrain ]; then
 #     gdown https://drive.google.com/uc?id=1qlgYaHXzCLmt_EpdIIIc0i4qu83rfqhp -O pretrain.zip
 # fi
 
-# checkpoint
+# if [ ! -d pretrain ]; then
+#     unzip pretrain.zip
+# fi
+
+
+# Prepare checkpoint
 if [ ! -d adapter_checkpoint ]; then
     gdown https://drive.google.com/uc?id=1XrA6szuQn_G2FoYhivu3knwX3j6VAMRa -O adapter_checkpoint.zip
+fi
+
+if [ ! -d adapter_checkpoint ]; then
+    unzip adapter_checkpoint.zip
 fi
--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# if [ ! -d data ]; then
-#     unzip data.zip
-# fi
-
-# if [ ! -d pretrain ]; then
-#     unzip pretrain.zip
-# fi
-
-if [ ! -d adapter_checkpoint ]; then
-    unzip adapter_checkpoint.zip
-fi
-
 python infer.py --base_model_path "${1}" \
                 --peft_path "${2}" \
                 --test_data_path "${3}" \
